ff2ce22a706cbf88f1a7aa126817a7985f948676
irlgabriel <radu.gabriel131@gmail.com>
2020-09-20 14:46:49 +0300

    added production build

---
+++ build/precache-manifest.8ee119088fdad4875be26390290748c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618a31b8c7ffb98ef85429bcd317ed62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce536305cdd6e3a9309a",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "8ad2d178a09c6708c6e2",
+    "url": "/static/js/2.dfdbcbf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfdbcbf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce536305cdd6e3a9309a",
+    "url": "/static/js/main.b26e3953.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
